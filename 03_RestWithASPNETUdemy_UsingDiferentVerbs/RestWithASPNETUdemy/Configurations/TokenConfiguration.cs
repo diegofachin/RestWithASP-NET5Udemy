5b9697dc76aafaa837e0cf0ec986d004ff6d85ab
diegofachin <diegofachin@gmail.com>
2021-04-02 20:55:58 -0300

    tentativa travis

--- 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/Configurations/TokenConfiguration.cs
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/Configurations/TokenConfiguration.cs
@@ -6,7 +6,6 @@
         public string Issuer { get; set; }
         public string Secret { get; set; }
         public int Minutes { get; set; }
-
         public int DaysToExpiry { get; set; }
     }
 }
